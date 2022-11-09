3c1acf8e55fc956a8fe9c1e2d147db47c9a3338a
wutsi <herve.tchepannou@wutsi.com>
2022-11-08 21:10:50 -0500

    add accountid

--- src/main/kotlin/com/wutsi/workflow/WorkflowContext.kt
+++ src/main/kotlin/com/wutsi/workflow/WorkflowContext.kt
@@ -1,7 +1,8 @@
 package com.wutsi.workflow
 
 class WorkflowContext(
-    val request: Any? = null
+    val request: Any? = null,
+    val accountId: Long? = null
 ) {
     var response: Any? = null
 }
