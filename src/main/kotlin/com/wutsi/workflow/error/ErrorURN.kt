0a280c41b16d1c1b5f7d8d71d25cf9f62f70664b
wutsi <herve.tchepannou@wutsi.com>
2022-11-21 14:25:34 -0500

    update errors

--- src/main/kotlin/com/wutsi/workflow/error/ErrorURN.kt
+++ src/main/kotlin/com/wutsi/workflow/error/ErrorURN.kt
@@ -5,10 +5,10 @@ enum class ErrorURN(val urn: String) {
 
     MEMBER_ALREADY_BUSINESS("urn:wutsi:error:member-already-business"),
     MEMBER_ALREADY_REGISTERED("urn:wutsi:error:member-already-registered"),
-    MEMBER_SUSPENDED("urn:wutsi:error:member-suspended"),
     MEMBER_NOT_ACTIVE("urn:wutsi:error:member-not-active"),
     MEMBER_NOT_BUSINESS("urn:wutsi:error:member-not-business-account"),
     MEMBER_NOT_FOUND("urn:wutsi:error:member-not-found"),
+    MEMBER_SUSPENDED("urn:wutsi:error:member-suspended"),
 
     NO_STORE("urn:wutsi:error:no-store"),
 
@@ -16,14 +16,14 @@ enum class ErrorURN(val urn: String) {
 
     PICTURE_LIMIT_REACHED("urn:wutsi:error:picture-limit-reached"),
 
-    PRODUCT_NOT_FOUND("urn:wutsi:error:product-not-found"),
-    PRODUCT_NOT_OWNER("urn:wutsi:error:product-not-owner"),
     PRODUCT_LIMIT_REACHED("urn:wutsi:error:product-limit-reached"),
+    PRODUCT_NOT_FOUND("urn:wutsi:error:product-not-found"),
+    PRODUCT_NO_STOCK("urn:wutsi:error:product-no-stock"),
+    PRODUCT_NOT_OWNER("urn:wutsi:error:product-not-owner"),
     PRODUCT_PICTURE_MISSING("urn:wutsi:error:product-pictures-missing"),
-    PRODUCT_NO_STOCK("urn:wutsi:error:product-no-store"),
 
     STORE_NOT_ACTIVE("urn:wutsi:error:store-not-active"),
-    STORE_NOT_SUPPORTED_IN_COUNTRY("urn:wutsi:error:store-not-supported-in-country"),
     STORE_NOT_FOUND("urn:wutsi:error:store-not-found"),
-    STORE_NOT_OWNER("urn:wutsi:error:store-not-owner")
+    STORE_NOT_OWNER("urn:wutsi:error:store-not-owner"),
+    STORE_NOT_SUPPORTED_IN_COUNTRY("urn:wutsi:error:store-not-supported-in-country")
 }
